578c0d27bdf024011b5318c1414b96d8d5c5b08f
Adam.Rackis <adam.rackis@riotgames.com>
2019-12-05 14:20:56 -0600

    tuple work

--- main.cpp
+++ main.cpp
@@ -1,17 +1,25 @@
 #include <vector>
 #include <initializer_list>
 #include <iostream>
-#include "vector/myVector.cpp"
-#include "vector/thing.cpp"
 
-#include "vector/test/filterMapReduce/lambda.cpp"
+#include "tuple/tuple.h"
 
-#include "vector/filter.h"
+using namespace rstl::Tuple;
 
 using std::cout;
 using std::endl;
 
 int main(int argc, const char *argv[])
 {
+    tuple t1 = make_tuple(1.1, 7.2, 9.3f);
+
+    auto d = get<1>(t1);
+    auto f = get<2>(t1);
+
+    std::cout << ((d == 7.2) ? "true" : "false") << ((d == 7) ? "true" : "false") << std::endl;
+    std::cout << ((f == 9.3f) ? "true" : "false") << ((f == 9) ? "true" : "false") << std::endl;
+
+    std::cout << get<0>(t1) << " " << get<1>(t1) << " " << get<2>(t1);
+
     return 0;
 }
---
+++ tuple/tuple.h
@@ -0,0 +1,60 @@
+namespace rstl
+{
+
+namespace Tuple
+{
+
+template <typename T, typename... Rest>
+class tuple
+{
+public:
+  tuple(T head, Rest... tail) : head(head), tail(tail...) {}
+
+  T head;
+
+  tuple<Rest...> tail;
+};
+
+template <typename T>
+class tuple<T>
+{
+public:
+  tuple(T head) : head(head) {}
+
+  T head;
+};
+
+template <typename T, typename... Rest>
+tuple<T, Rest...> make_tuple(T head, Rest... tail)
+{
+  return tuple(head, tail...);
+}
+
+template <unsigned I, typename Head, typename... Rest>
+struct tuple_element
+{
+  using Type = typename tuple_element<I - 1, Rest...>::Type;
+  tuple_element(const tuple<Head, Rest...> &t)
+  {
+    val = tuple_element<I - 1, Rest...>{t.tail}.val;
+  }
+  Head val;
+};
+
+template <typename Head, typename... Rest>
+struct tuple_element<0, Head, Rest...>
+{
+  using Type = Head;
+  tuple_element(const tuple<Head, Rest...> &t) : val(t.head) {}
+  Head val;
+};
+
+template <unsigned I, typename... Types>
+typename tuple_element<I, Types...>::Type get(const tuple<Types...> &t)
+{
+  return tuple_element<I, Types...>(t).val;
+}
+
+} // namespace Tuple
+
+} // namespace rstl
