4293c98712b4655c14f4dbae4c6d7ede531dcac7
Adam.Rackis <adam.rackis@riotgames.com>
2019-12-05 10:27:58 -0600

    code structure cleanup

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <initializer_list>
+#include <iostream>
+#include "vector/myVector.cpp"
+#include "vector/thing.cpp"
+
+#include "vector/test/filterMapReduce/lambda.cpp"
+
+#include "vector/filter.h"
+
+using std::cout;
+using std::endl;
+
+int main(int argc, const char *argv[])
+{
+    return 0;
+}
